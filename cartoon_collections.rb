850c8fce9b62a19d45007f9d5cf3f77270059beb
Matt Ericksen <ericksenm93@ymail.com>
2020-07-26 23:30:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,8 +22,6 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
   #planeteer_calls.select { |ele| valid_calls.include?(ele) }
   #planeteer_calls[0]
-  planeteer_calls.find do |ele|
-    valid_calls.include?(ele)
-  end
-  planeteer_calls[0]
+  planeteer_calls.find { |ele| valid_calls.include?(ele) }
+  #planeteer_calls[0]
 end
